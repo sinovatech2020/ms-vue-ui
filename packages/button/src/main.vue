7c1422c8ee830ae460df949c49937f658ad19d62
helinyan <helinyan@sinovatech.com>
2020-07-27 13:07:50 +0800

    新增

--- packages/button/src/main.vue
+++ packages/button/src/main.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="btn-wrapper" :style="bg" @click="handleClick">
-    <span>{{msg}}</span>
+    <span class="btn-text" :style="{fontSize:fontSize}">{{msg}}</span>
   </div>
 </template>
 <script>
@@ -18,27 +18,38 @@ export default {
     type: {
       default: "default",
       type: String
-    }
+    },
+    width: String,
+    height: String,
+    fontSize: String
   },
   computed: {
     bg() {
       if (this.disable && this.type === "default") {
         return {
-          background: "#ff6024"
+          background: "#ff6024",
+          width: this.width,
+          height: this.height
         };
       } else if (this.disable && this.type === "empty") {
         return {
           border: "1px solid #ff6024",
-          color: "#ff6024"
+          color: "#ff6024",
+          width: this.width,
+          height: this.height
         };
       } else if (!this.disable && this.type === "default") {
         return {
-          background: "#ccc"
+          background: "#ccc",
+          width: this.width,
+          height: this.height
         };
       } else {
         return {
           border: "1px solid #ccc",
-          color: "#ccc"
+          color: "#ccc",
+          width: this.width,
+          height: this.height
         };
       }
     }
--- src/app.vue
+++ src/app.vue
@@ -1,76 +1,60 @@
 <template>
-  <div class="text">
-    <button @click="handleClick">点击</button>
-    <ms-area-picker v-model="show" @confirm='confirm' :pickerData='pickerData' :selected='selected'></ms-area-picker>
-    <p>省:{{provices.value}}--{{provices.text}}</p>
-    <p>市:{{citys.value}}--{{citys.text}}</p>
-    <p>区:{{areas.value}}--{{areas.text}}</p>
+  <div class="un-scroll-box">
+    <ms-scroll @pullUpFun='pullUpFun' @pullDownFun='pullDownFun' ref='myScroll'>
+      <div v-for="(item,index) in list" :key="index">
+        <div class="item">{{item}}</div>
+      </div>
+    </ms-scroll>
   </div>
 </template>
 <script>
-import "../packages/theme/src/areaPicker.scss";
+import '../packages/theme/src/scroll.scss'
+const arr = () => {
+  return [...new Array(30).keys()];
+};
 export default {
   data() {
     return {
-      pickerData: [],
-      show: false,
-      provices: {},
-      citys: {},
-      areas: {},
-      selected: []
+      list: []
     };
   },
   methods: {
-    handleClick() {
-      this.show = true;
+    pullDownFun(cb) {
+      setTimeout(() => {
+        this.list = arr();
+        cb();
+      }, 2000);
     },
-    confirm(data) {
-      this.provices = data[0];
-      this.citys = data[1];
-      this.areas = data[2];
-      console.log(data);
+    pullUpFun(cb) {
+      console.log(arr());
+      setTimeout(() => {
+        this.list = this.list.concat(arr());
+        cb(false);
+      }, 1000);
     }
   },
   mounted() {
-    setTimeout(() => {
-      this.selected = [4, 1, 2];
-      this.pickerData = all();
-      console.log(this.pickerData)
-    }, 2000);
+    // setTimeout(()=>{
+      this.list=arr()
+      this.$nextTick(()=>{
+        this.$refs.myScroll.bscroll.refresh();
+      })
+    // },2000)
   }
-};
-const randomFun = function() {
-  return Math.floor(Math.random() * 5);
-};
-const all = function() {
-  let provice = ["北京", "上海", "广州", "山东", "天津"];
-  let values = ["011", "012", "013", "014", "015"];
-  let citys = ["城市1", "城市2", "城市3", "城市4", "城市5"];
-  let areas = ["区域1", "区域2", "区域3", "区域4", "区域5"];
-  let arr = [];
-  for (let i = 0; i < 10; i++) {
-    let provices = {
-      provice: provice[randomFun()],
-      value: values[randomFun()],
-      cityes: []
-    };
-    for (let j = 0; j < 6; j++) {
-      let citye = {
-        text: citys[randomFun()],
-        value: values[randomFun()],
-        areas: []
-      };
-      provices.cityes.push(citye);
-      for (let k = 0; k < 6; k++) {
-        let area = {
-          text: areas[randomFun()],
-          value: values[randomFun()]
-        };
-        provices.cityes[j].areas.push(area);
-      }
-    }
-    arr.push(provices);
-  }
-  return arr;
 };
 </script>
+<style lang="scss" scoped>
+.un-scroll-box {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+.item {
+  width: 100%;
+  height: 30px;
+  margin-bottom: 20px;
+  background: #ccc;
+}
+</style>
